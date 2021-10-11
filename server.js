01650ffeb668e7161a49b009414c756981fd2d12
RedBlood639 <forever78419@gmail.com>
2021-10-11 08:34:15 +0000

    feture : basic graphsql

--- server.js
+++ server.js
@@ -1,20 +1,51 @@
-var { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema } = require('graphql')
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+const schema = buildSchema(
+  `
+        type Query {
+            hello : String
+            quoteOfTheDay : String
+            random : Float!
+            rollThreeDice : [Int]
+            rollDice(numDice: Int!, numSides: Int): [Int]
+        }
+    `,
+)
 
-// The root provides a resolver function for each API endpoint
-var root = {
+const root = {
   hello: () => {
-    return 'Hello world!'
+    return 'Hello world'
+  },
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+  },
+  random: () => {
+    return Math.random()
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6))
+  },
+
+  rollDice: ({ numDice, numSides }) => {
+    var output = []
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
+    }
+    return output
   },
 }
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response)
-})
+const app = express()
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  }),
+)
+
+app.listen(4000)
+console.log('Running a GraphQL API server at http://localhost:4000/graphql')
