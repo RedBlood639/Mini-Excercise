424530f9d2229ddb5f9f9cf68526bbfb104b9022
RedBlood639 <forever78419@gmail.com>
2021-10-20 14:38:37 +0800

    feature : add customize admin panel

--- django-todo-react/backend/todo/admin.py
+++ django-todo-react/backend/todo/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Todo
 
 class TodoAdmin(admin.ModelAdmin):
+
+        list_filter = ['completed']
+        search_fields = ['title']
         list_display = ('title','description','completed')
 
 # Register your models here.
